8360658f774bb92e958244e56aa0557b02b56db7
renmurase <renmurase@gmail.com>
2025-07-08 03:15:42 +0900

    first commit

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    public void main(String[] args) {
+        int num1=1;
+        int num2=2;
+        int num3=3;
+
+        System.out.printf("%d%d%d\n",num1,num2,num3);
+        System.out.println("Hello, World!");
+    }
+}
