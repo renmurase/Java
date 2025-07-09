45c8d3f42128b62e0892c3a73488e40e8e4dfe7e
renmurase <renmurase@gmail.com>
2025-07-10 01:09:15 +0900

    add loop massage

--- test.java
+++ test.java
@@ -1,10 +1,21 @@
 public class test {
     public void main(String[] args) {
-        int num1=1;
-        int num2=2;
-        int num3=3;
+        int num1=4;
+        int num2=6;
+        int ans=0;
 
-        System.out.printf("%d%d%d\n",num1,num2,num3);
-        System.out.println("Hello, World!");
+        ans=calculation(num1, num2);
+        //System.out.println("Hello, World!");
+        for(int i=0;i<3;i++){
+            System.out.println("いらっしゃいませ、こんにちは");
+        }
+        System.out.printf("\n%d+%dの答えは%dです\n\n",num1,num2,ans);
+    }
+
+    private int calculation(int num1, int num2) {
+
+        int ans=0;
+        ans=num1+num2;
+        return ans;
     }
 }
